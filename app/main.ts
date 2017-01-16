753f339bbc69933de84cc84262f42a8616f8263b
zhuhjtcx@qq.com <zzbj891016>
2017-01-16 10:13:40 +0800

    ts

---
+++ app/main.ts
@@ -0,0 +1,9 @@
+/**
+ * Created by Administrator on 2017/1/16.
+ */
+
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app.module';
+
+const platform = platformBrowserDynamic();
+platform.bootstrapModule(AppModule);
